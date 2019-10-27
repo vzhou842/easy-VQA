f023f436d0d955f8dce57aa161bdef5fdc8ed0a7
Victor Zhou <vzhou842@gmail.com>
2019-10-27 15:32:55 -0700

    Add JSON output for questions

--- generate_data.py
+++ generate_data.py
@@ -3,19 +3,25 @@ from color import Color
 from images import createImage
 from questions import createQuestions
 from random import choice
+import json
 
 colors = list(Color)
 shapes = list(Shape)
 
 allQuestions = []
 
-for i in range(10):
+NUM_IMAGES = 10
+
+for i in range(NUM_IMAGES):
   shape = choice(shapes)
   color = choice(colors)
 
   createImage(i, shape, color)
 
-  questions = map(lambda x: x + (i,), createQuestions(shape, color))
+  questions = list(map(lambda x: x + (i,), createQuestions(shape, color)))
   allQuestions += questions
 
-print(allQuestions)
+with open('data/questions.json', 'w') as file:
+  json.dump(questions, file)
+
+print(f'Generated {NUM_IMAGES} images and {len(allQuestions)} questions.')
