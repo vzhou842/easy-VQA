d42d2044bf781b0eb9339d3e7a87342a5aafbb68
Victor Zhou <vzhou842@gmail.com>
2019-10-26 20:50:30 -0700

    Implement createCircleImage

--- draw.py
+++ draw.py
@@ -3,23 +3,36 @@ from random import randint
 
 IM_SIZE = 64
 
-MIN_RECTANGLE_SIZE = 5
+MIN_SHAPE_SIZE = 5
 
 def createRectangleImage(filename):
-  # Initialize a blank white image
   im = Image.new('RGBA', (IM_SIZE, IM_SIZE), (255, 255, 255, 255))
 
   draw = ImageDraw.Draw(im)
 
   x = randint(0, IM_SIZE / 2)
   y = randint(0, IM_SIZE / 2)
-  w = randint(MIN_RECTANGLE_SIZE, IM_SIZE / 2)
-  h = randint(MIN_RECTANGLE_SIZE, IM_SIZE / 2)
+  w = randint(MIN_SHAPE_SIZE, IM_SIZE / 2)
+  h = randint(MIN_SHAPE_SIZE, IM_SIZE / 2)
   draw.rectangle([(x, y), (x + w, y + h)], fill=(255, 0, 0))
 
   del draw
-
-  # write to stdout
   im.save(filename, 'PNG')
 
-createRectangleImage('test.png')
+
+def createCircleImage(filename):
+  im = Image.new('RGBA', (IM_SIZE, IM_SIZE), (255, 255, 255, 255))
+
+  draw = ImageDraw.Draw(im)
+
+  x = randint(0, IM_SIZE / 2)
+  y = randint(0, IM_SIZE / 2)
+  r = randint(MIN_SHAPE_SIZE, IM_SIZE / 2 - 1)
+  draw.ellipse([(x, y), (x + r, y + r)], fill=(255, 0, 0))
+
+  del draw
+  im.save(filename, 'PNG')
+
+
+createRectangleImage('rectangle.png')
+createCircleImage('circle.png')
