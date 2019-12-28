72a09cf088b373af2caa3ac84c12095e9af91437
Victor Zhou <vzhou842@gmail.com>
2019-12-28 22:35:36 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,15 +8,17 @@ setuptools.setup(
     version="0.0.1",
     author="Victor Zhou",
     author_email="vzhou842@gmail.com",
-    description="A wrapper for the easy-VQA dataset.",
+    description="The official package for the easy-VQA dataset.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="http://github.com/vzhou842/easy-VQA",
+    project_urls={
+      'Demo': 'https://easy-vqa-demo.victorzhou.com',
+    }
     packages=setuptools.find_packages(),
-    install_requires=[
-    ],
     include_package_data=True,
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
